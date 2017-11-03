4c7ad810f3a26a1e517ac581484bec90e40934d5
toys416 <toys416@gmail.com>
2017-11-03 11:07:51 +0800

    mvn init

---
+++ src/main/java/com/zuora/ApiAutoFramework/App.java
@@ -0,0 +1,13 @@
+package com.zuora.ApiAutoFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
