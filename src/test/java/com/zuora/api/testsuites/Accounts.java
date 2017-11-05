b98cffcc99edd14b2f4ba47233f69fa130a95379
toys416 <toys416@gmail.com>
2017-11-05 15:15:06 +0800

    add framwork

--- src/test/java/com/zuora/api/testsuites/Accounts.java
+++ src/test/java/com/zuora/api/testsuites/Accounts.java
@@ -1,32 +1,8 @@
 package com.zuora.api.testsuites;
 
-import java.lang.reflect.Method;
-
-import org.testng.Assert;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import com.zuora.api.utils.LogHelper;
+public class Accounts {
 
-
-public class Accounts extends BaseTest{
-
-
-	@BeforeMethod(alwaysRun = true)
-	public void setUp(Method method) throws Exception {
-	}
-
-	@AfterMethod
-	public void teardown(ITestResult itr) throws Exception {
-
-	}
-
-	@Test(testName = "Create_Account", groups = { "create" })
-	public void create_account() throws Exception {
-		LogHelper.debug("first case");
-		Assert.assertEquals(1, 1);
-	}
+	
 }
